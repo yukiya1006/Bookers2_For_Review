8601670eaf62c2e16e1b63432e2f8d3ae536b3c1
yukiya1006 <96877368+yukiya1006@users.noreply.github.com>
2022-03-06 16:02:35 +0000

    Start

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,78 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+# findメソッドでモデルと紐づくテーブルのDBからbookのレコードを1つ取得
+# newメソッドでbookコメントを作成
+  def show
+    @book = Book.find(params[:id])
+    @book_comment = BookComment.new
+  end
+
+# allメソッドでモデルと紐づくbookのすべてのレコードを取得
+# newメソッドでbookを作成(bookクラスをインスタンス化)
+
+  def index
+     @book = Book.new
+  if params[:button].to_i == 1
+     @books = Book.all.order(created_at: :desc)
+    # button1の処理
+  elsif params[:button].to_i == 2
+     @books = Book.all.order(star: :desc)
+    # button2の処理
+  else
+     @books = Book.all
+  end
+  end
+
+# newメソッドでbookクラスをインスタンス化(カラムを引数として渡す)
+# user_id = current_user.idでログインしている本人かどうか確認
+# saveメソッドでbook作成
+# renderはコントローラーを介さず直接viewを表示させるため表示先の変数を表記
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = User.new
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :star)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+end
---
+++ app/models/book.rb
@@ -0,0 +1,24 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+	
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+end
